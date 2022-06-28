cab7322d0976693f16ca7509803ca1ae95634184
Jason Stepp <hi@jasonstepp.me>
2022-06-27 18:19:19 -0700

    Added new expense form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Card from "./components/UI/Card"
 import Expenses from "./components/Expenses/Expenses"
+import NewExpense from "./components/NewExpense/NewExpense"
 
 function App() {
 	return (
 		<div>
+			<Card>
+				<NewExpense />
+			</Card>
 			<Card>
 				<Expenses></Expenses>
 			</Card>
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import ExpenseForm from "./ExpenseForm"
+
+function NewExpense() {
+	return (
+		<div className="expense-form">
+			<ExpenseForm />
+		</div>
+	)
+}
+
+export default NewExpense
